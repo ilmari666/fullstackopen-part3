21fb018efea95ab0cbda51c13352e1d41f3fbcb9
Ilmari <ilmari@sokeri.com>
2018-05-24 23:03:48 +0300

    add linting rules to backend

--- backend/index.js
+++ backend/index.js
@@ -6,31 +6,15 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 
 const Person = require('./models/person');
-const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('build'));
 
-morgan.token('body', function(req, res) {
-  return JSON.stringify(req.body);
-});
+morgan.token('body', req => JSON.stringify(req.body));
 
-app.use(
-  morgan(':method :url :body :status :res[content-length] - :response-time ms')
-);
-
-const addNewPerson = function(name, number) {
-  const person = {
-    name,
-    number,
-    created: new Date().getTime(),
-    id: getId()
-  };
-  persons.push(person);
-  return person;
-};
+app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'));
 
 const respondWithError = (response, statusCode, message) => {
   response.status(statusCode);
@@ -38,10 +22,8 @@ const respondWithError = (response, statusCode, message) => {
 };
 
 app.get('/info', (request, response) => {
-  Person.find({}).then(result => {
-    const message = `<html>puhelinluettelossa on ${
-      result.length
-    } henkilön tiedot<br/>${new Date().toString()}</html>`;
+  Person.find({}).then((result) => {
+    const message = `<html>puhelinluettelossa on ${result.length} henkilön tiedot<br/>${new Date().toString()}</html>`;
     response.send(message);
   });
 });
@@ -51,80 +33,74 @@ app.get('/api/persons', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
+  const { id } = request.params;
   Person.findById(id)
     .then(Person.format)
     .then(response.json)
-    .catch(e =>
-      respondWithError(response, 404, `No user exists with id ${id}`)
-    );
+    .catch(() =>
+      respondWithError(response, 404, `No user exists with id ${id}`));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response) => { // eslint-disable-line
   const { name, number } = request.body;
   if (!name) {
-    return respondWithError(response, 400, `Name must be defined.`);
+    return respondWithError(response, 400, 'Name must be defined.');
   }
   if (!number) {
-    return respondWithError(response, 400, `Number must be defined.`);
+    return respondWithError(response, 400, 'Number must be defined.');
   }
   if (name && number) {
     Person.findOne({ name })
-      .then(result => {
-        console.log(result);
+      .then((result) => {
         if (result !== null) {
           throw new Error('Name must be unique');
         }
         const person = new Person({
           name,
           number,
-          created: new Date()
+          created: new Date(),
         });
-        person.save()
+        person
+          .save()
           .then(Person.format)
-          .then(formattedPerson => {
+          .then((formattedPerson) => {
             response.status(201);
             response.json(formattedPerson);
           });
       })
-      .catch(e => {
-        return respondWithError(response, 409, 'Name must be unique');
-      });
+      .catch(() => respondWithError(response, 409, 'Name must be unique'));
   }
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
+  const { id } = request.params;
   Person.findByIdAndDelete(id)
-    .then(queryResponse => response.status(204).end())
-    .catch(e =>
-      respondWithError(response, 404, `No user exists with id ${id}`)
-    );
+    .then(() => response.status(204).end())
+    .catch(() => respondWithError(response, 400, 'Bad request'));
 });
 
 app.put('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
+  const { id } = request.params;
   const { name, number } = request.body;
   if (name && number) {
     Person.findOneAndUpdate(id, { name, number })
       .then(Person.format)
       .then(response.json)
-      .catch(e => respondWithError(response, 400, `Bad request`));
+      .catch(() => respondWithError(response, 400, 'Bad request'));
   } else {
-    respondWithError(response, 400, `Bad request`);
+    respondWithError(response, 400, 'Bad request');
   }
 });
 
 app.patch('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
+  const { id } = request.params;
   // no validation of patch data can overwrite anything
   const patch = request.body;
   Person.findOneAndUpdate(id, patch)
     .then(Person.format)
     .then(response.json)
-    .catch(e =>
-      respondWithError(response, 404, `No user exists with id ${id}`)
-    );
+    .catch(() =>
+      respondWithError(response, 404, `No user exists with id ${id}`));
 });
 
 // catch unhandled requests
@@ -135,5 +111,5 @@ const error = (request, response) => {
 app.use(error);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`); //eslint-disable-line
 });
